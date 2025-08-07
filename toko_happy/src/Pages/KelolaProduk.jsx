6d1fc6f7702e0fea54f896c7bafe981d2046720a
Opjasson <azmitheusos333@gmail.com>
2025-08-07 19:11:38 +0700

    frontend : setting template kelola product

--- toko_happy/src/Pages/KelolaProduk.jsx
+++ toko_happy/src/Pages/KelolaProduk.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import MainLayout from '../Components/Templates/MainLayout'
+import React from "react";
+import MainLayout from "../Components/Templates/MainLayout";
 
 const KelolaProduk = () => {
-  return (
-    <MainLayout>
-      <h1>Kelola produk</h1>
-    </MainLayout>
-  )
-}
+    return (
+        <MainLayout>
+            <h1 className="text-2xl font-bold">Kelola Produk</h1>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <button className="bg-green-400 px-4 py-2 text-black font-bold rounded cursor-pointer hover:bg-green-500">
+                    + Tambah Produk
+                </button>
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Nama Produk
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Stok
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Kategori
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Harga
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Aksi
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                            <th
+                                scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Microsoft Surface Pro
+                            </th>
+                            <td class="px-6 py-4">White</td>
+                            <td class="px-6 py-4">Laptop PC</td>
+                            <td class="px-6 py-4">$1999</td>
+                            <td class="px-6 py-4">
+                                <a
+                                    href="#"
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-5">
+                                    Edit
+                                </a>
+                                <a
+                                    href="#"
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                    Hapus
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </MainLayout>
+    );
+};
 
-export default KelolaProduk
+export default KelolaProduk;
