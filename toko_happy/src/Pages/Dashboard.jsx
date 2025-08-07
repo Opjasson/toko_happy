fc40607fd1eb44b5d91f6ce0f2572dde9e69c770
Opjasson <azmitheusos333@gmail.com>
2025-08-07 18:55:04 +0700

    frontend : add table in Dashboard page

--- toko_happy/src/Pages/Dashboard.jsx
+++ toko_happy/src/Pages/Dashboard.jsx
@@ -3,12 +3,52 @@ import MainLayout from '../Components/Templates/MainLayout'
 
 const Dashboard = () => {
   return (
-    <MainLayout>
-      <div>
-        Ini adalah isi
-      </div>
-    </MainLayout>
-  )
+      <MainLayout>
+          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                      <tr>
+                          <th scope="col" class="px-6 py-3">
+                              Nama Produk
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                              Stok
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                              Kategori
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                              Harga
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                              Aksi
+                          </th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                          <th
+                              scope="row"
+                              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                              Microsoft Surface Pro
+                          </th>
+                          <td class="px-6 py-4">White</td>
+                          <td class="px-6 py-4">Laptop PC</td>
+                          <td class="px-6 py-4">$1999</td>
+                          <td class="px-6 py-4">
+                              <a
+                                  href="#"
+                                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                  Tambahkan
+                              </a>
+                          </td>
+                      </tr>
+                      
+                  </tbody>
+              </table>
+          </div>
+      </MainLayout>
+  );
 }
 
 export default Dashboard
