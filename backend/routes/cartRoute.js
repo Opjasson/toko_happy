cb1d48820e3b81d28a8ae6bebe688d6d77c3fda8
Opjasson <azmitheusos333@gmail.com>
2025-08-10 14:06:36 +0700

    backend : add update cart route

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addCart, getCartAll } from "../controllers/cartControllers.js";
+import { addCart, getCartAll, updateCart } from "../controllers/cartControllers.js";
 
 const route = express.Router();
 
 route.get("/cart", getCartAll);
 route.post("/cart", addCart);
+route.patch("/cart/:id", updateCart);
 
 export default route;
