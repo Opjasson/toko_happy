3024ed23f650e81ce50afb01c51837eec745c4e2
Opjasson <azmitheusos333@gmail.com>
2025-08-10 14:53:47 +0700

    frontend : create tambah Produk page

---
+++ toko_happy/src/Pages/TambahProduk.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../Components/Templates/MainLayout'
+
+const TambahProduk = () => {
+  return (
+    <MainLayout>
+      
+    </MainLayout>
+  )
+}
+
+export default TambahProduk
